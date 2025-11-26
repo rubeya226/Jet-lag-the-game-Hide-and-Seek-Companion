bba7570d878274023054e1e4f7cd19ab392d2fdb
Brennan Duman <dumanb@oregonstate.edu>
2025-11-26 11:41:40 -0800

    full implementation of updateMessages()

--- mssg-ret.js
+++ mssg-ret.js
@@ -1,7 +1,9 @@
+const flipNum = 4;
+
 async function getMessageChange(){
     let localMssgCount = document.getElementsByClassName("message").length;
 
-    let mssgData = await fetch("http://flip4.engr.oregonstate.edu:6327/message-change");
+    let mssgData = await fetch("http://flip" + flipNum + ".engr.oregonstate.edu:6327/message-change");
     let json = await mssgData.json();
     let mssgCount = json.count;
 
@@ -14,12 +16,52 @@ async function getMessageChange(){
     
 }
 
-async function updateMessages(){
+async function getMessages(){
+    let messageData = await fetch("http://flip" + flipNum + ".engr.oregonstate.edu:6327/messages");
+    let json = await messageData.json();
+    return await json.messages;
+}
+
+function getMessageContainer(){
+    return document.getElementsByClassName("messages")[0];
+}
+
+function getDisplayedMessagesCount(container){
+    return container.getElementsByClassName("message").length;
+}
+
+function createMessage(role, container, message){
+    let newMssg = document.createElement("div");
+    newMssg.classList.add("message");
+    newMssg.textContent = message.content;
+
+    if(message.sender == "hider"){
+        newMssg.classList.add("hider");
+        if(role == "hider"){
+            newMssg.classList.add("self");
+        }else{
+            newMssg.classList.add("opp");
+        }
+    }else{
+        newMssg.classList.add("seeker");
+        if(role == "hider"){
+            newMssg.classList.add("opp");
+        }else{
+            newMssg.classList.add("self");
+        }
+    }
+
+    container.appendChild(newMssg);
+}
+
+async function updateMessages(role){
     if(await getMessageChange()){
-        //getMessages()
-        //getOldMessages()
-        //for(let i = oldMssgs.length; i < mssgs.length; i++){
-        //  createMessage(mssgs[i]);
-        //}
+        let mssgs = await getMessages();
+        let container = getMessageContainer();
+        let displayedMssgCount = getDisplayedMessagesCount(container);
+
+        for(let i = displayedMssgCount; i < mssgs.length; i++){
+            createMessage(role, container, mssgs[i]);
+        }
     }
 }
