cd3b1b423621377a663c83dddc06a6856c758cb7
Joesph Haney <haneyjoe@flip2.engr.oregonstate.edu>
2025-12-06 17:08:16 -0800

    Working Timer that saves between screens

--- static/hider.js
+++ static/hider.js
@@ -1,11 +1,16 @@
+
+var filePath = "/time.json"
+
 function chat(){
     window.location.href = "/chat/hider";
 }
+
 var display = document.getElementById('timer-text')
-var spent_time = 0
-var start_time
+
+var spent_time = parseInt(localStorage.getItem("time")) || 0
+var start_time = parseInt(localStorage.getItem("start_time")) || Date.now()
 var timer_interval
-var timer_running = false
+var timer_running = localStorage.getItem("timer_run") === "true"
 var timer_done = false
 function time_format(ms){
     const hours = Math.floor(ms/3600000)
@@ -20,6 +25,8 @@ function timer_start(){
     if(!timer_running){
         timer_running = true
         start_time = Date.now() - spent_time
+        localStorage.setItem("start_time", start_time)
+        localStorage.setItem("timer_run", true)
         timer_interval = setInterval(timer, 1000)
     }
 }
@@ -29,22 +36,43 @@ function timer_pause(){
         timer_running = false
         clearInterval(timer_interval)
         spent_time = Date.now() - start_time;
-
+        localStorage.setItem("time", spent_time)
+        localStorage.setItem("timer_run", false)
     }
     
 }
 function timer_end(){
     console.log("timer_end called")
-    if(timer_running){
-        timer_running = false
-        clearInterval(timer_interval)
-        spent_time = Date.now() - start_time;
+    timer_pause()
+    localStorage.setItem("time", 0)
+    spent_time = 0
+    localStorage.setItem("start_time", Date.now())
+    start_time = Date.now()
+    console.log("time updated to: "+localStorage.getItem("time"))
+    display.textContent = "Hiding Time: 00:00:00"
 
-    }
 }
-
+window.onload = () => {
+    if (start_time && timer_running) {
+        timer_interval = setInterval(timer, 1000)
+        spent_time = parseInt(localStorage.getItem("time"))
+    }
+    display.textContent = "Hiding Time: "+time_format(parseInt(localStorage.getItem("time")))
+}
+document.addEventListener("visibilitychange", () => {
+    if (!document.hidden && timer_running) {
+        spent_time = parseInt(localStorage.getItem("time"))
+        display.textContent = "Hiding Time: " + time_format(spent_time)
+        console.log("timer remembered?")
+    }
+})
 function timer(){
+    if(!timer_running) return
     spent_time = Date.now() - start_time
+    localStorage.setItem("time", spent_time)
+    localStorage.setItem("start_time", start_time)
+    localStorage.setItem("timer_run", timer_running)
+    console.log("time stored in localStorage")
     display.textContent = "Hiding Time: "+time_format(spent_time)
-    console.log("time updated to: "+time_format(spent_time))
+    console.log("time updated to: "+parseInt(localStorage.getItem("time")))
 }
